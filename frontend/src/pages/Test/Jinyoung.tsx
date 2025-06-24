b74c4b59a7d3ce9599542b70b6306217fa678313
Jung Jinyoung <ya_1001@naver.com>
2025-06-25 02:43:15 +0900

    feat : 상황별 컴포넌트 예시

--- frontend/src/pages/Test/Jinyoung.tsx
+++ frontend/src/pages/Test/Jinyoung.tsx
@@ -3,16 +3,28 @@ import { CertificateCard } from '@/features/Home/ui/card/CertificateCard';
 import SectionBlock from '@/features/Home/ui/SectionBlock';
 import { certificateList } from '@/features/Home/data/cerificateList';
 
+import { reviewData } from '@/features/GapReview/data/reviewList';
+import ReviewCard from '@/features/GapReview/ui/reviewCard';
+
+import { TimelineCard } from '@/features/Home/ui/card/TimelineCard';
 export default function Jinyoung() {
   return (
     <>
-      <h1>로그인한 유저의 맞춤형 데이터가 있을 경우</h1>
-      <div className="w-[842px]">
+      <div className="w-[842px] mx-auto flex flex-col gap-10 my-10">
         <SectionBlock title="맞춤 자격증 추천">
           {certificateList.map((certificate, index) => (
             <CertificateCard key={certificate.name + index} {...certificate} />
           ))}
         </SectionBlock>
+        <div className="flex flex-col gap-4">
+          <ReviewCard type="purple" {...reviewData} />
+          <ReviewCard type="skyblue" {...reviewData} />
+          <ReviewCard type="yellow" {...reviewData} />
+          <ReviewCard type="green" {...reviewData} />
+        </div>
+        <div>
+          <TimelineCard />
+        </div>
       </div>
     </>
   );
