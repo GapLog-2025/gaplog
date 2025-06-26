76858c99ad13df0764ae8881ef5c77b82385dfa5
Jung Jinyoung <ya_1001@naver.com>
2025-06-26 19:56:51 +0900

    style : ActionButton 스타일 변경 및 className 변수 적용

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -8,7 +8,7 @@ type MoveButtonProps = {
 
 function MoveButton({ type = 'default', children, onClick }: MoveButtonProps) {
   const baseClasses =
-    'w-fit bg-white border-2 typo-strong rounded-lg px-4 py-2';
+    'w-fit bg-white border-2 typo-strong rounded-lg px-5 py-2';
   const primaryClasses =
     'border border-primary-primary-background text-primary';
   const defaultClasses = 'border border-border text-main';
@@ -25,18 +25,19 @@ function MoveButton({ type = 'default', children, onClick }: MoveButtonProps) {
 type ActionButtonProps = {
   size?: 'small' | 'large';
   children: React.ReactNode;
+  className?: string;
   onClick?: () => void;
 };
 function ActionButton({
   size = 'small',
   children,
+  className = '',
   onClick,
 }: ActionButtonProps) {
-  const baseClasses =
-    size === 'small' ? 'px-4 py-2' : 'min-w-[220px] px-4 py-2';
+  const baseClasses = size === 'small' ? 'px-5 py-2' : 'px-10 py-4';
   return (
     <Button
-      className={` ${baseClasses} w-fit bg-primary-action text-white typo-strong rounded-lg`}
+      className={` ${baseClasses} w-fit bg-primary-action text-white typo-strong rounded-lg ${className}`}
       onClick={onClick}
     >
       {children}
